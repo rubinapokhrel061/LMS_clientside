9d7396a8384efce6fa1506c9dd78afc618eaddc3
rubinapokhrel061 <pokhrelrubina2061.com>
2024-06-11 22:13:59 +0545

    url change

--- src/Pages/config.js
+++ src/Pages/config.js
@@ -1,2 +1,2 @@
-export const backendUrl = "https://lms-server-v1j9.onrender.com";
+export const backendUrl = "https://lms-serverside.onrender.com";
 // export const backendUrl = "http://localhost:3000";
